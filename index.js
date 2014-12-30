f2af1371b5b3bf45b83d67006c48b4a197386f97
Jeff Andersen <jeffandersen@gmail.com>
2014-12-30 13:15:38 -0400

    Allow cors for client/api communication

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routing
-app.use('/', router);
+app.use('/', allowCors, router);
+
+function allowCors(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+};
 
 // error handling
 errors.notfound(app);
